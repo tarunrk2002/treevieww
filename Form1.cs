8c6ee95ba66551e36684c4fc43a7f5f587ceb0c3
tarun <tarunrk2002@gmail.com>
2024-07-01 18:20:28 +0530

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace treeview
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            if (treeView1.Nodes.Count == 0)
+            {
+                treeView1.Nodes.Add("root");
+            }
+            treeView1.Nodes[0].Nodes.Add("newfromroot");
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            treeView1.SelectedNode.Remove();
+        }
+    }
+}
